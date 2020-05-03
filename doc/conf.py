jjasonkal/ckan

Deployment doc little improvements
* fixed instruction of reloading nginx, when it should be restarting -
you need to start nginx before it will let us reload it. (It doesn't
start when you install it, because apache is listening on port 80 by
default when you installed it)
* service -> systemctl - because systemd replaced upstart in ubuntu
since 16.04, and people are very unlikely to be installing on 14.04 now.
* rst syntax corrections

doc/conf.py
@@ -55,8 +55,8 @@ rst_epilog = '''
 .. |storage_parent_dir| replace:: /var/lib/ckan
 .. |storage_dir| replace:: |storage_parent_dir|/default
 .. |storage_path| replace:: |storage_parent_dir|/default
-.. |reload_apache| replace:: sudo service apache2 reload
-.. |restart_apache| replace:: sudo service apache2 restart
+.. |reload_apache| replace:: sudo systemctl reload apache2
+.. |restart_apache| replace:: sudo systemctl restart apache2
 .. |restart_solr| replace:: sudo service jetty8 restart
 .. |solr| replace:: Solr
 .. |restructuredtext| replace:: reStructuredText
@@ -67,7 +67,7 @@ rst_epilog = '''
 .. |javascript| replace:: JavaScript
 .. |apache| replace:: Apache
 .. |nginx_config_file| replace:: /etc/nginx/sites-available/ckan
-.. |reload_nginx| replace:: sudo service nginx reload
+.. |reload_nginx| replace:: sudo systemctl restart nginx
 .. |jquery| replace:: jQuery
 .. |nodejs| replace:: Node.js
 

xtymac/opendata-platform

doc/conf.py
@@ -55,8 +55,8 @@ rst_epilog = '''
 .. |storage_parent_dir| replace:: /var/lib/ckan
 .. |storage_dir| replace:: |storage_parent_dir|/default
 .. |storage_path| replace:: |storage_parent_dir|/default
-.. |reload_apache| replace:: sudo service apache2 reload
-.. |restart_apache| replace:: sudo service apache2 restart
+.. |reload_apache| replace:: sudo systemctl reload apache2
+.. |restart_apache| replace:: sudo systemctl restart apache2
 .. |restart_solr| replace:: sudo service jetty8 restart
 .. |solr| replace:: Solr
 .. |restructuredtext| replace:: reStructuredText
@@ -67,7 +67,7 @@ rst_epilog = '''
 .. |javascript| replace:: JavaScript
 .. |apache| replace:: Apache
 .. |nginx_config_file| replace:: /etc/nginx/sites-available/ckan
-.. |reload_nginx| replace:: sudo service nginx reload
+.. |restart_nginx| replace:: sudo systemctl restart nginx
 .. |jquery| replace:: jQuery
 .. |nodejs| replace:: Node.js
 